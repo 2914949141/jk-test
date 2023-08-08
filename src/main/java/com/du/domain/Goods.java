a50120c5783c821e7ddf4fe306f9f0ef5f78da84
dujx <dujx@stpass.com>
2023-08-08 11:02:02 +0800

    second commit

--- src/main/java/com/du/domain/Goods.java
+++ src/main/java/com/du/domain/Goods.java
@@ -1,12 +1,12 @@
-package com.du.domain;
-
-import cn.afterturn.easypoi.excel.annotation.Excel;
-
-public class Goods {
-
-    @Excel(name  = "商品编号", orderNum = "0", format = "isAddIndex")
-    private Integer index = 1;
-
-    @Excel(name = "商品名称 *")
-    private String name;
-}
+//package com.du.domain;
+//
+//import cn.afterturn.easypoi.excel.annotation.Excel;
+//
+//public class Goods {
+//
+//    @Excel(name  = "商品编号", orderNum = "0", format = "isAddIndex")
+//    private Integer index = 1;
+//
+//    @Excel(name = "商品名称 *")
+//    private String name;
+//}
--- src/main/java/com/du/domain/User.java
+++ src/main/java/com/du/domain/User.java
@@ -1,35 +1,35 @@
-package com.du.domain;
-
-import cn.afterturn.easypoi.excel.annotation.Excel;
-import cn.afterturn.easypoi.excel.annotation.ExcelCollection;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.List;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class User {
-
-    @Excel(name = "序号", orderNum = "0", format = "isAddIndex")
-    private Integer index = 1;
-
-    @Excel(name = "用户id *")
-    private String id;
-
-    @Excel(name = "用户id *")
-    private String username;
-
-    @Excel(name = "性别 *", replace = {"男_1", "女_0"})
-    private Integer age;
-
-    @ExcelCollection(name = "商品信息")
-    private List<Goods> goodsList;
-
-    public User(String id, String username) {
-        this.id = id;
-        this.username = username;
-    }
-}
+//package com.du.domain;
+//
+//import cn.afterturn.easypoi.excel.annotation.Excel;
+//import cn.afterturn.easypoi.excel.annotation.ExcelCollection;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.List;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class User {
+//
+//    @Excel(name = "序号", orderNum = "0", format = "isAddIndex")
+//    private Integer index = 1;
+//
+//    @Excel(name = "用户id *")
+//    private String id;
+//
+//    @Excel(name = "用户id *")
+//    private String username;
+//
+//    @Excel(name = "性别 *", replace = {"男_1", "女_0"})
+//    private Integer age;
+//
+//    @ExcelCollection(name = "商品信息")
+//    private List<Goods> goodsList;
+//
+//    public User(String id, String username) {
+//        this.id = id;
+//        this.username = username;
+//    }
+//}
